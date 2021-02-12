7d762812b6b4c0c65c450e27c3270996017675e2
Suryam26 <suryam.jain@gmail.com>
2021-02-12 12:08:28 +0530

    API added and working

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,21 +5,20 @@ import {  Card, CardImg, CardText, CardBody,
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookmark } from '@fortawesome/free-regular-svg-icons';
 
-class InfoCard extends React.Component {
-    render() {
+function InfoCard ({ movie }) {
         return (
-            <Card color="dark" className="col-3 m-3">
+            <Card color="dark">
+                <CardImg top width="100%" src={`https://image.tmdb.org/t/p/w500/${ movie.poster_path }`} alt={ movie.title } />
                 <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <CardTitle tag="h5">{ movie.title }</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">Release Date: { movie.release_date }</CardSubtitle>
+                    <CardText>{ movie.overview }</CardText>
                     <Button className="px-3">
                         <FontAwesomeIcon icon={faBookmark} />
                     </Button>
                 </CardBody>
             </Card>            
         );
-    }
 }
 
 export default InfoCard;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Navbar, Input, Button, Row } from "reactstrap";
+import { Container, Navbar, Input, Button, Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 import InfoCard from "./Card";
@@ -9,16 +9,36 @@ import InfoCard from "./Card";
 class Search extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value : '' };
-
+        this.state = {
+            value: '',
+            Movies : [],
+        };
+        
         this.handleChange = this.handleChange.bind(this);
+        this.getMovies = this.getMovies.bind(this);
     }
 
     handleChange(event) {
         this.setState({
             value: event.target.value
         });
+        this.getMovies(this.state.value);
     }
+
+    async getMovies(query) {
+        if (query.length > 1) {
+            const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`);
+            const data = await response.json();
+            this.setState({
+                Movies : data.results,
+            })
+        }
+        else{
+           this.setState({
+                Movies : [],
+            }) 
+        }
+  }
 
     render() {
         return (
@@ -36,7 +56,11 @@ class Search extends React.Component {
 
                 <Container>
                     <Row>
-                        <InfoCard />
+                        {this.state.Movies.map(movie => (
+                                <Col key={movie.id} sm="12" md="6" lg="4" className="my-3 mx-auto">
+                                    <InfoCard movie={movie} />
+                                </Col>
+                        ))}
                     </Row>
                 </Container>
             </>
